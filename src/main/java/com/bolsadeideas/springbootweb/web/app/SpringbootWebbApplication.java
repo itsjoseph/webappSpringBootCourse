b5c0b8c7a4ad5a38806a2d0b766fbc874d1cd07b
amnesia <amnesia@2806-107e-0014-182a-92a5-c1aa-815f-84e3.ipv6.infinitum.net.mx>
2024-06-13 18:03:33 -0600

    git init

---
+++ src/main/java/com/bolsadeideas/springbootweb/web/app/SpringbootWebbApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springbootweb.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bolsadeideas/springbootweb/web/app/SpringbootWebbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springbootweb.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
