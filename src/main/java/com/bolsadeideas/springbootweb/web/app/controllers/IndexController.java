2fb1bc466ab62c912ff4786fc046555058f477fd
amnesia <amnesia@2806-107e-0014-182a-92a5-c1aa-815f-84e3.ipv6.infinitum.net.mx>
2024-06-13 18:43:10 -0600

    Objeto Model

--- src/main/java/com/bolsadeideas/springbootweb/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springbootweb/web/app/controllers/IndexController.java
@@ -1,16 +1,28 @@
 package com.bolsadeideas.springbootweb.web.app.controllers;
 
+import com.bolsadeideas.springbootweb.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @RequestMapping(value = {"/index","/","/home"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/index", "/", "/home"}, method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("titulo", "Bienvenido a Spring Boot");
         return "index";
+    }
+
+    @RequestMapping(value = "/detalle", method = RequestMethod.GET)
+    public String about(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setName("Juan");
+        usuario.setLastName("Perez");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Bienvenido a usuario: " + usuario.getName());
+        return "perfil";
     }
 }
