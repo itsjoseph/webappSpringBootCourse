a3f540a82506663dc7cdf4a4e2ef7ee7af3bf160
amnesia <amnesia@2806-107e-0014-182a-92a5-c1aa-815f-84e3.ipv6.infinitum.net.mx>
2024-06-13 19:09:43 -0600

    Implementacion de each tymeleaf

--- src/main/java/com/bolsadeideas/springbootweb/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springbootweb/web/app/controllers/IndexController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
@@ -21,8 +24,20 @@ public class IndexController {
         Usuario usuario = new Usuario();
         usuario.setName("Juan");
         usuario.setLastName("Perez");
+        /*usuario.setEmail("juan.perez@gmail.com");*/
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Bienvenido a usuario: " + usuario.getName());
         return "perfil";
+    }
+
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String listar(Model model) {
+        List<Usuario> listar = Arrays.asList(new Usuario("luis", "Amador", "corre.com"),
+                new Usuario("luis", "Amador", "corre.com"),
+                new Usuario("luis", "Amador", "corre.com"),
+                new Usuario("luis", "Amador", "corre.com"));
+        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("usuarios", listar);
+        return "listar";
     }
 }
--- src/main/java/com/bolsadeideas/springbootweb/web/app/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springbootweb/web/app/models/Usuario.java
@@ -3,6 +3,17 @@ package com.bolsadeideas.springbootweb.web.app.models;
 public class Usuario {
     private String name;
     private String lastName;
+    private String email;
+
+
+    public Usuario() {
+    }
+
+    public Usuario(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
@@ -18,5 +29,13 @@ public class Usuario {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
